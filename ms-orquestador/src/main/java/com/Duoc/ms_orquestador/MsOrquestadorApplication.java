517fe02c934ac8c475b45ded49d63ff6405b11d0
MechaTeemo1412 <alanvidal1412@gmail.com>
2026-04-30 09:19:32 -0400

    Chore:Setup Spring Boot base folders for the 5 microservices

---
+++ ms-orquestador/src/main/java/com/Duoc/ms_orquestador/MsOrquestadorApplication.java
@@ -0,0 +1,13 @@
+package com.Duoc.ms_orquestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrquestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrquestadorApplication.class, args);
+	}
+
+}
---
+++ ms-orquestador/src/test/java/com/Duoc/ms_orquestador/MsOrquestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Duoc.ms_orquestador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrquestadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
