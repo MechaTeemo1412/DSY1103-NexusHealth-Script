b75c6f18494241874de5ba1d91a00dfc88f5ac83
MechaTeemo1412 <alanvidal1412@gmail.com>
2026-05-01 15:41:05 -0400

    refactor: change to read-only Repository interface to prevent accidental writes

--- ms-pacientes/src/main/java/com/Duoc/ms_pacientes/repository/PacienteRepository.java
+++ ms-pacientes/src/main/java/com/Duoc/ms_pacientes/repository/PacienteRepository.java
@@ -5,9 +5,12 @@ import com.Duoc.ms_pacientes.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+    List<Paciente> findAll();
+    Optional<Paciente>findById(Long id);
     Optional<Paciente>findByRut(String rut);
 }
