6b6df9a6ffee68f787de032a8d03eab2e2750be8
MechaTeemo1412 <alanvidal1412@gmail.com>
2026-05-01 15:20:39 -0400

    feat:add CRUD endpoints and Oracle Cloud connection for ms-pacientes

---
+++ ms-pacientes/src/main/java/com/Duoc/ms_pacientes/controller/PacienteController.java
@@ -0,0 +1,44 @@
+package com.Duoc.ms_pacientes.controller;
+
+import com.Duoc.ms_pacientes.model.Paciente;
+import com.Duoc.ms_pacientes.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
+
+public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> listarPacientes(){
+        return new ResponseEntity<>(pacienteService.obtenerTodos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> obtenerPaciente(@PathVariable Long id){
+        return pacienteService.obtenerPorId(id)
+                .map(paciente -> new ResponseEntity<>(paciente,HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> crearPaciente(@Valid @RequestBody Paciente paciente){
+        Paciente nuevoPaciente = pacienteService.guardarPaciente(paciente);
+        return new ResponseEntity<>(nuevoPaciente,HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarPaciente(@PathVariable Long id){
+        pacienteService.eliminarPaciente(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ ms-pacientes/src/main/java/com/Duoc/ms_pacientes/model/Paciente.java
@@ -0,0 +1,26 @@
+package com.Duoc.ms_pacientes.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+
+@Table(name="pacientes")
+
+public class Paciente {
+    @Id
+    @Column(name="ID_PACIENTE")
+    private Long id;
+
+    @Column(name="RUT_PACIENTE",length = 12)
+    private String rut;
+
+    @Column(name="NOMBRE_COMPLETO",length = 100)
+    private String nombre;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    @Column(name="TELEFONO_CONTACTO")
+    private String telefono;
+}
