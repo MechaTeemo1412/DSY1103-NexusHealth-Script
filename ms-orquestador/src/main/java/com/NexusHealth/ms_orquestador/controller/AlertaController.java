324704a1acb80289a00d86de6a34e078dfd6c080
alanvidalh <ala.vidalh@duocuc.cl>
2026-06-14 18:20:11 -0400

    feat: implementing Beans of swagger configuration on ms-orquestador

--- ms-orquestador/src/main/java/com/NexusHealth/ms_orquestador/controller/AlertaController.java
+++ ms-orquestador/src/main/java/com/NexusHealth/ms_orquestador/controller/AlertaController.java
@@ -2,6 +2,12 @@ package com.NexusHealth.ms_orquestador.controller;
 
 import com.NexusHealth.ms_orquestador.model.Alerta;
 import com.NexusHealth.ms_orquestador.service.AlertaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/alertas")
+@Tag(name = "Orquestador",description = "Controlador Central de tareas automatizadas y manuales")
 public class AlertaController {
     @Autowired
     private AlertaService alertaService;
     @PostMapping("/ejecutar-ahora")
+    @Operation(
+            summary = "Disparar alertas (manualmente)",
+            description = "Endspoint para ejecutar bajo demanda de flujo completo: lee agenda, valida con pacientes, envía por notificaciones y reporta a auditoría. Posee tolerancia a fallos."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Flujo finalizado",
+                    content = @Content(mediaType = "applicaion/json",schema = @Schema(implementation = Alerta.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Error o timeout de algún microservicio",
+                    content = @Content
+            )
+    })
     public ResponseEntity<Alerta> dispararProcesoManualmente() {
         return ResponseEntity.ok(alertaService.procesarAlertasDeAgenda());
     }
