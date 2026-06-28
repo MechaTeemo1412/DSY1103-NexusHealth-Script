99ec6642847f3664f56bfe475dd53881928e78bb
alanvidalh <ala.vidalh@duocuc.cl>
2026-06-27 22:00:34 -0400

    fixing some syntax errors

--- ms-notificaciones/src/main/java/com/NexusHealth/ms_notificaciones/feignclient/AuditoriaClient.java
+++ ms-notificaciones/src/main/java/com/NexusHealth/ms_notificaciones/feignclient/AuditoriaClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="ms-auditoria",url="url = \"${auditoria.url}")
+@FeignClient(name="ms-auditoria",url= "${auditoria.url}")
 public interface AuditoriaClient {
     @PostMapping("/registro")
     void registrarEvento(@RequestBody LogAuditoriaDTO notificacion);
